5ed2f984a932f14e440f2d412297a92b1208ac8e
daodaoajl <144930715+daodaoajl@users.noreply.github.com>
2023-10-05 15:41:01 +0900

    2

--- main.c
+++ main.c
@@ -5,15 +5,15 @@ int main(int argc, char *argv[])
 {
   int x;
   
-  printf("정수를 입력하시오 : ");
+  printf("input number : ");
   scanf("%i",&x);
   
   if(x > 0)
-       { printf("양수입니다.\n"); }
+       { printf("absolute value is %i.\n", x); }
   else if(x == 0)
-       { printf("0 입니다.\n"); }
+       { printf("absolute value is %i.\n", x); }
   else if(x < 0)
-       { printf("음수입니다.\n"); }
+       { printf("absolute value is %i.\n", -x); }
   
   system("PAUSE");	
   return 0;
